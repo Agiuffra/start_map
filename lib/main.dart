8a12e8e6899db11520559b2d7b673797d00bd012
Camilo Reynaga <camilo.reynaga@gmail.com>
2020-10-14 02:17:29 -0500

    stack para full screenmap

--- lib/main.dart
+++ lib/main.dart
@@ -25,22 +25,22 @@ class PuntodePartida extends StatefulWidget {
 }
 
 class _PuntodePartidaState extends State<PuntodePartida> {
-  var Lati = 0.0;
-  var Long = 0.0;
+  var latitud = 0.0;
+  var longitud = 0.0;
 
   void _getCurrentLocation() async {
     final position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
     setState(() {
-      Lati = position.latitude;
-      print(Lati);
-      Long = position.longitude;
-      print(Long);
+      latitud = position.latitude;
+      print(latitud);
+      longitud = position.longitude;
+      print(longitud);
     });
     Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => FullScreenMap(Lat: Lati,Lon: Long)
+          builder: (context) => FullScreenMap(Lat: latitud,Lon: longitud)
         ));
   }
 
@@ -94,104 +94,7 @@ class _PuntodePartidaState extends State<PuntodePartida> {
           ),
         ),
         body: FullScreenMap(),
-        floatingActionButton: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(16,44,84,1),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  alignment: Alignment.center,
-                  child: IconButton(
-                    alignment: Alignment.center,
-                    icon: Icon(Icons.gps_fixed),
-                    iconSize: 30,
-                    color: Colors.white,
-                    onPressed: (){
-                      _getCurrentLocation();
-                    },
-                  ),
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.only(left: 10, right:10,),
-                            height: 35,
-                            width: 320,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(width: 2, color: Color.fromRGBO(215,215,215,1)),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: TextFormField(
-                              decoration: const InputDecoration(
-                                border: InputBorder.none,
-                                hintText: 'Ingrese dirección',
-                              ),
-                              style: GoogleFonts.montserrat(
-                                fontSize: 15.0,
-                                color: Color.fromRGBO(168,168,168,1),
-                                fontWeight: FontWeight.w400,
-                              ),
-                              validator: (value) {
-                                if (value.isEmpty) {
-                                  return 'Please enter some text';
-                                }
-                                return null;
-                              },
-                            ),
-                          ),
-                          Icon(
-                            Icons.search,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 16.0, bottom: 25),
-                      child: RaisedButton(
-                        onPressed: (){},
-                        textColor: Color.fromRGBO(16,44,84,1),
-                        padding: EdgeInsets.all(0.0),
-                        child: Container(
-                          width: 206,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(255,240,0,1),
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          padding: EdgeInsets.only(left: 71, right: 71, top: 9, bottom: 9),
-                          child: Text(
-                            'Confirmar',
-                            style: GoogleFonts.montserrat(
-                              fontWeight: FontWeight.w400,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
+
       ),
     );
   }
@@ -215,18 +118,124 @@ class _FullScreenMapState extends State<FullScreenMap> {
 
   @override
   Widget build(BuildContext context) {
-    return crearMapa();
-  }
+    return new Scaffold(
+      body: Stack(
+        children: [
 
-  MapboxMap crearMapa() {
-    return MapboxMap(
-      onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: LatLng(widget.Lat,widget.Lon),
-        zoom: 14,
+        MapboxMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(widget.Lat,widget.Lon),
+          zoom: 14,
+        ),
       ),
+          Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(16,44,84,1),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        alignment: Alignment.center,
+                        child: IconButton(
+                          alignment: Alignment.center,
+                          icon: Icon(Icons.gps_fixed),
+                          iconSize: 30,
+                          color: Colors.white,
+                          onPressed: (){
+                            // _getCurrentLocation();
+                          },
+                        ),
+                      ),
+                    ),
+                    Form(
+                      // key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.only(left: 10, right:10,),
+                                  height: 35,
+                                  width: 320,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(width: 2, color: Color.fromRGBO(215,215,215,1)),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Ingrese dirección',
+                                    ),
+                                    style: GoogleFonts.montserrat(
+                                      fontSize: 15.0,
+                                      color: Color.fromRGBO(168,168,168,1),
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return 'Please enter some text';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.search,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16.0, bottom: 25),
+                            child: RaisedButton(
+                              onPressed: (){},
+                              textColor: Color.fromRGBO(16,44,84,1),
+                              padding: EdgeInsets.all(0.0),
+                              child: Container(
+                                width: 206,
+                                height: 35,
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(255,240,0,1),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                padding: EdgeInsets.only(left: 71, right: 71, top: 9, bottom: 9),
+                                child: Text(
+                                  'Confirmar',
+                                  style: GoogleFonts.montserrat(
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+          ),
+        ],
+      ) ,
     );
   }
+
+
 }
 
 class CustomAppBar extends CustomClipper<Path> {
